3af50839e0fcfe700c4216fdbb310483296de82a
thibault vanwynendaele <thibault.vanwynendaele@live.fr>
2019-08-18 22:46:33 +0200

    fast timer

--- frontend/src/app/game-timer/game-timer.component.ts
+++ frontend/src/app/game-timer/game-timer.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { timer, interval, Observable, Subscribable, Subscription } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-game-timer',
-  templateUrl: './game-timer.component.html',
-  styleUrls: ['./game-timer.component.scss']
+    selector: 'app-game-timer',
+    templateUrl: './game-timer.component.html',
+    styleUrls: ['./game-timer.component.scss']
 })
 export class GameTimerComponent implements OnInit {
 
-  constructor() { }
+    count: number;
+    source: Observable<number>;
+    timer$: Observable<number>;
+    counter$: Observable<number>;
+    subscribe: Subscription;
 
-  ngOnInit() {
-  }
+    @Output('timeEnd') timeEnd = new EventEmitter<boolean>();
+
+    constructor() {
+        this.count = 5;
+        this.source = interval(1000);
+        this.timer$ = timer(6000);
+
+        this.counter$ = this.source.pipe(
+            map(() => this.count--),
+            takeUntil(this.timer$)
+        );
+    
+        this.subscribe = this.counter$.subscribe(() => { if (this.count <= 0) { this.timeEnd.emit(true);} });
+    }
+
+    ngOnInit() {
+        
+    }
+
+    
+
+    
+
 
 }
--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GameActionComponent } from '../game-action/game-action.component';
 import { GameTimerComponent } from '../game-timer/game-timer.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -8,10 +9,18 @@ import { GameTimerComponent } from '../game-timer/game-timer.component';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-  
-  constructor() { }
+
+  resultPhase: boolean;
+
+  constructor() {
+    this.resultPhase = false;
+   }
 
   ngOnInit() {
+  }
+
+  timeEnd(isTimeEnded) {
+    this.resultPhase = isTimeEnded;
   }
 
 }
